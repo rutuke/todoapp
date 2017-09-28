e5397d1c9f12fca2846f29791f2b4ff77dbc568d
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-09-28 20:34:35 +0100

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,12 @@
+//checking off todos by clicking
+$("li").click(function(){
+    // if li is gray turn it black, else turn gray
+    if($(this).css("color") === "gray"){
+
+    }
+    $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    });
+});
+
