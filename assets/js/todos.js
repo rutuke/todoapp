aa71ee53a5973604123c0dee0470a68abd85dd71
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-09-30 22:14:19 +0100

    final commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,29 @@
 //checking off todos by clicking
-$("li").click(function(){
-    // if li is gray turn it black, else turn gray
-    if($(this).css("color") === "gray"){
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
 
-    }
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
+// deleting function of X
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        // removes the li instead of just diplay:none
+        $(this).remove();
     });
+    //stop the event from bubbling up - only works on the span
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // assigning the new to do
+        var newTodo = $(this).val();
+        $(this).val("");
+        // adding new li to ul
+        $("ul").append("<li><i class='fa fa-trash-o' aria-hidden='true'></i> "+ newTodo +"</li>");
+    }
+});
+
+$(".fa-pencil-square-o").click(function(){
+    $("input[type='text'").fadeToggle();
 });
 
