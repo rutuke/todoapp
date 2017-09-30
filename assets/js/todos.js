74ba63dfe0b213385a5d0e6abdc0c95d39565b78
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-09-30 22:47:14 +0100

    commit 30/09

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,43 +1,25 @@
-//checking off todos by clicking
 $("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
-// deleting function of X
+//Click on X to delete Todo
 $("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500,function(){
-        // removes the li instead of just diplay:none
         $(this).remove();
     });
-    //stop the event from bubbling up - only works on the span
     event.stopPropagation();
 });
 
 $("input[type='text']").keypress(function(event){
     if(event.which === 13){
-        // assigning the new to do
-        var newTodo = $(this).val();
+        //grabbing new todo text from input
+        var todoText = $(this).val();
         $(this).val("");
-        // adding new li to ul
-        $("ul").append("<li><i class='fa fa-trash-o' aria-hidden='true'></i> "+ newTodo +"</li>");
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 });
 
 $(".fa-pencil-square-o").click(function(){
-    $("input[type='text'").fadeToggle();
+    $("input[type='text']").fadeToggle()
 });
-
-=======
-//checking off todos by clicking
-$("li").click(function(){
-    // if li is gray turn it black, else turn gray
-    if($(this).css("color") === "gray"){
-        $(this).css("color", "gray");
-    }
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-    });
-});
-
->>>>>>> db8eaaf624152d9e74c483d31ea916abd80319d8
